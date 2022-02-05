6a03fe6ac1d4bc2d1de69715ddda47715d91dffb
Danny <89036003+daboucha@users.noreply.github.com>
2022-02-05 15:05:26 -0500

    Update of html & app.py files

--- app.py
+++ app.py
@@ -6,11 +6,14 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
-def index(score='', data=[]):
-    if request.method == "POST":
+def index(score = '', data=[]):
+    if request.method == "POST":  
         data = request.form.getlist('players')
+        
         y_pred = predictScore(data)
-        score = y_pred
-
+        score=y_pred
+    
     return render_template('index.html', score=score, data=data, len=len(data))
 
+if __name__=='__main__':
+    app.run()
